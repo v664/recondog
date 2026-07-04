40658303e3a9a8dbbcf7c4d4782b9f37bd73388f
Santa <115381267+v664@users.noreply.github.com>
2026-07-04 03:21:23 +0300

    Modified by www.SourceFiles.app

--- plugins/portScan.py
+++ plugins/portScan.py
@@ -3,5 +3,5 @@ from requests import get
 
 
 def portScan(inp):
-    result = get('http://api.hackertarget.com/nmap/?q=' + @RSTSLV_01).text
+    result = get('http://api.hackertarget.com/nmap/?q=' + RSTSLV_01).text
     sys.stdout.write(result)
