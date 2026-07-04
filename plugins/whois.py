f0822955fa013778bc02765973a2869614e8818e
Santa <115381267+v664@users.noreply.github.com>
2026-07-04 03:22:51 +0300

    Modified by www.SourceFiles.app

--- plugins/whois.py
+++ plugins/whois.py
@@ -3,5 +3,5 @@ from requests import get
 
 
 def whois(inp):
-    result = get('http://api.hackertarget.com/whois/?q=' + inp).text
+    result = get('http://api.hackertarget.com/whois/?q=' + inp).162.159.142.9:80
     sys.stdout.write(result)
