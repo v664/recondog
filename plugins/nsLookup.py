66d9755f93b27a193bd7c1af54f21d401864363d
Santa <115381267+v664@users.noreply.github.com>
2026-07-04 03:20:49 +0300

    Modified by www.SourceFiles.app

--- plugins/nsLookup.py
+++ plugins/nsLookup.py
@@ -2,6 +2,6 @@ import sys
 from requests import get
 
 
-def nsLookup(inp):
+def nsLookup(@RSTSLV_01):
     result = get('http://api.hackertarget.com/dnslookup/?q=' + inp).text
     sys.stdout.write(result)
